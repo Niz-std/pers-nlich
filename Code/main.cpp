1bbe127f1b457726b3144205f748623195d10ff2
Osef Alfan Fadhil <osefalfan.f@gmail.com>
2020-01-22 00:01:03 +0700

    +Revisi

--- Code/main.cpp
+++ Code/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-  std::cout << "Hello World!";              //prints Hello World!
-  std::cout << "Ich bin ein C++ programme"; //prints Ich bin ein C++ programme
+  cout << "Hello World!";              //prints Hello World!
+  cout << "Ich bin ein C++ programme"; //prints Ich bin ein C++ programme
   return 0;
 }
