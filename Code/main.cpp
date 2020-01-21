94cb1f301b567f315b137c7cd8b11aa809d28ecd
Osef Alfan Fadhil <osefalfan.f@gmail.com>
2020-01-21 22:11:48 +0700

    +Simplifikasi syntax (using implicit/qualified/namespace) +html :+preprocessor

---
+++ Code/main.cpp
@@ -0,0 +1,11 @@
+/*  my second program in C++
+    with more comments */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  std::cout << "Hello World!";              //prints Hello World!
+  std::cout << "Ich bin ein C++ programme"; //prints Ich bin ein C++ programme
+}
